f0d66b20f7e16163d16a44fbb4c1dd90754c10f9
Samuel Kirori <samuelkirori300@gmail.com>
2024-03-30 13:48:31 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function (fare){
+        return fare*integer;
+    }
+}
+function fareDoubler(fare){  
+    return fare*2;
+}
+
+function fareTripler(fare){
+    return fare*3;
+}
+
+
+function selectDifferentDrivers(drivers,selectingDrivers){
+    return selectingDrivers(drivers);
+
+}
